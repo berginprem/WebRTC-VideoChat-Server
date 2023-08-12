e5ac16833d3eff3186aee33cee6b7eeb9b5a40c0
berginprem <76146666+berginprem@users.noreply.github.com>
2023-08-12 08:01:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const corsOptions =  {
+	origin: "https://warm-treacle-ee789c.netlify.app/g",
+	methods: [ "GET", "POST" ]
+}
+const io = require("socket.io")(server, {
+	cors: corsOptions,
+});
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.send('Running');
+});
+
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+module.exports = app;
